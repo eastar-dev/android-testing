e1f29a1bcfdc2001894ef132828f1d3e54f5bb7d
eastar <eastarj@gmail.com>
2021-04-01 23:13:46 +0900

    code : single game view model

--- tdd/src/main/java/dev/eastar/tdd/main/SingleViewModel.kt
+++ tdd/src/main/java/dev/eastar/tdd/main/SingleViewModel.kt
@@ -2,6 +2,14 @@ package dev.eastar.tdd.main
 
 import androidx.lifecycle.ViewModel
 
-class SingleViewModel : ViewModel() {
+class SingleViewModel constructor(val gameRepository: GameRepository) : ViewModel() {
     // TODO: Implement the ViewModel
+
+    init {
+        gameRepository.generatRandomNumber()
+    }
+
+    fun tryNumber(number: Int, who: String = "") {
+        gameRepository.tryNumberResult(number, who)
+    }
 }

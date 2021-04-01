bc9e3e2c72d82e3fd59db857c44ec532b7323317
eastar <pluto@eastandroid.com>
2021-04-01 17:36:39 +0900

    red
    java.lang.AssertionError:
    Expected: is <100>
         but: was <99>
    Expected :is <100>
    Actual   :<99>

--- tdd/src/test/java/dev/eastar/tdd/data/GeneratorRandomNumberSourceImplTest.kt
+++ tdd/src/test/java/dev/eastar/tdd/data/GeneratorRandomNumberSourceImplTest.kt
@@ -1,8 +1,11 @@
 package dev.eastar.tdd.data
 
 import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.collection.IsIn
 import org.junit.Test
+import java.lang.Integer.max
+import java.lang.Math.min
 
 class GeneratorRandomNumberSourceImplTest {
 
@@ -10,9 +13,28 @@ class GeneratorRandomNumberSourceImplTest {
     fun getRandomNumber1between100() {
         //given
         val generatorRandomNumberSource = GeneratorRandomNumberSourceImpl()
+        repeat(10000) {
+            //when
+            val actual: Int = generatorRandomNumberSource.getRandomNumber1between100()
+            //then
+            MatcherAssert.assertThat(actual, IsIn((1..100).toList()))
+        }
+    }
+
+    @Test
+    fun getManyRandomNumberMin1Max100() {
+        //given
+        val generatorRandomNumberSource = GeneratorRandomNumberSourceImpl()
         //when
-        val actual: Int = generatorRandomNumberSource.getRandomNumber1between100()
+        var min = Int.MAX_VALUE
+        var max = Int.MIN_VALUE
+        repeat(100000) {
+            val actual: Int = generatorRandomNumberSource.getRandomNumber1between100()
+            min = min(min, actual)
+            max = max(max, actual)
+        }
         //then
-        MatcherAssert.assertThat(actual, IsIn((1..100).toList()))
+        MatcherAssert.assertThat(min, `is`(1))
+        MatcherAssert.assertThat(max, `is`(100))
     }
 }

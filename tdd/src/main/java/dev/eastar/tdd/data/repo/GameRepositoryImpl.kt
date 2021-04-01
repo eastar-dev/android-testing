6f122505899b128d492bb69d0182f8cf2e8f0c14
eastar <eastarj@gmail.com>
2021-04-01 23:23:10 +0900

    code green  : impl + test init

---
+++ tdd/src/main/java/dev/eastar/tdd/data/repo/GameRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package dev.eastar.tdd.data.repo
+
+class GameRepositoryImpl : GameRepository {
+    override fun generateRandomNumber() {
+        TODO("Not yet implemented")
+    }
+
+    override fun tryNumberResult(number: Int, who: String) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ tdd/src/test/java/dev/eastar/tdd/data/repo/GameRepositoryImplTest.kt
@@ -0,0 +1,16 @@
+package dev.eastar.tdd.data.repo
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GameRepositoryImplTest {
+
+    @Test
+    fun generateRandomNumber() {
+    }
+
+    @Test
+    fun tryNumberResult() {
+    }
+}
